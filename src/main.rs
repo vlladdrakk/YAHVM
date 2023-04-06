29b7ddc8819cf093dfd1e96bfb31f2224840c979
Thomas Tramley <thomas@caddieservice.co>
2023-04-06 19:23:49 -0300

    Added complexity so we can have short versions of instructions later

--- src/main.rs
+++ src/main.rs
@@ -5,95 +5,189 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use phf::{phf_map};
-use std::process::exit;
 
 static OPCODES : phf::Map<&'static str, &'static str> = phf_map! {
-    "PRT" => "0000",
-    "SET" => "0001",
-    "ADD" => "0010",
-    "SUB" => "0011",
-    "MUL" => "0100",
-    "DIV" => "0101",
-    "JMP" => "0110",
-    "JNP" => "0111",
-    "EQL" => "1000",
-    "CBP" => "1001",
-    "CLP" => "1010",
+  "PRT" => "0000",
+  "SET" => "0001",
+  "ADD" => "0010",
+  "SUB" => "0011",
+  "MUL" => "0100",
+  "DIV" => "0101",
+  "JMP" => "0110",
+  "JNP" => "0111",
+  "EQL" => "1000",
+  "CBP" => "1001",
+  "CLP" => "1010",
 };
 
 static VARS : phf::Map<&'static str, &'static str> = phf_map! {
-    "$0" => "0000",
-    "$2" => "0001",
-    "$3" => "0010",
-    "$4" => "0011",
-    "$5" => "0100",
-    "$6" => "0101",
-    "$7" => "0110",
-    "$8" => "0111",
-    "$9" => "1000",
-    "$a" => "1001",
-    "$b" => "1011",
-    "$c" => "1100",
-    "$d" => "1101",
-    "$e" => "1110",
-    "$f" => "1111",
+  "$0" => "0000",
+  "$2" => "0001",
+  "$3" => "0010",
+  "$4" => "0011",
+  "$5" => "0100",
+  "$6" => "0101",
+  "$7" => "0110",
+  "$8" => "0111",
+  "$9" => "1000",
+  "$a" => "1001",
+  "$b" => "1011",
+  "$c" => "1100",
+  "$d" => "1101",
+  "$e" => "1110",
+  "$f" => "1111",
 };
 
 static TYPE : phf::Map<&'static str, &'static str> = phf_map! {
-    "0" => "00",
-    "1" => "01",
-    "2" => "10",
-    "3" => "11",
+  "0" => "00",
+  "1" => "01",
+  "2" => "10",
+  "3" => "11",
 };
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
+struct Instruction {
+  opcode: String,
+  var: String,
+  ins_type: String,
+  num: String,
+}
 
-    run(file_path);
+impl Instruction {
+  fn default() -> Instruction {
+    return Instruction {
+      opcode: String::new(),
+      var: String::new(),
+      ins_type: String::new(),
+      num: String::new(),
+    }
+  }
+  fn parse_opcode(&mut self, opcode: &str) {
+    self.opcode = String::from(OPCODES[opcode]);
+  }
+
+  fn parse_opcode_from_line(&mut self, line: String) {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    if parts.len() > 0 {
+      self.parse_opcode(parts[0]);
+    } else {
+      println!("ERROR: Provided line didn't contain any opcode");
+    }
+  }
+
+  fn parse_var(&mut self, var: &str) {
+    self.var = String::from(VARS[&var.to_lowercase()]);
+  }
+
+  fn parse_var_from_line(&mut self, line: String) {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    if parts.len() > 1 {
+      self.parse_var(parts[1]);
+    } else {
+      println!("ERROR: Provided line didn't contain any var");
+    }
+  }
+
+  fn parse_type(&mut self, raw_type: &str) {
+    self.ins_type = String::from(TYPE[raw_type]);
+  }
+
+  fn parse_type_from_line(&mut self, line: String) {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    if parts.len() > 2 {
+      self.parse_type(parts[2]);
+    } else {
+      println!("ERROR: Provided line didn't contain any type");
+    }
+  }
+
+  fn parse_num(&mut self, num: &str) {
+    let mut number : String = String::new();
+
+    if num.starts_with('-') {
+      number = String::from("1");
+    }
+
+    number.push_str(&format!("{:08b}", num.parse::<i32>().unwrap()));
+
+    self.num = number;
+  }
+
+  fn parse_num_from_line(&mut self, line: String) {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    if parts.len() > 1 {
+      self.parse_num(parts[3]);
+    } else {
+      println!("ERROR: Provided line didn't contain any num");
+    }
+  }
+
+  fn parse(&mut self, line: String) {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    self.parse_opcode(parts[0]);
+    self.parse_var(parts[1]);
+    self.parse_type(parts[2]);
+    self.parse_num(parts[3]);
+  }
+
+  fn parse_from_vec(&mut self, parts: Vec<&str>) {
+    self.parse_opcode(parts[0]);
+    self.parse_var(parts[1]);
+    self.parse_type(parts[2]);
+    self.parse_num(parts[3]);
+  }
+
+  fn as_binary(&self) -> String {
+    return String::from(format!("{}{}{}{}", self.opcode, self.var, self.ins_type, self.num));
+  }
+
+  fn line_to_binary(&mut self, line: String) -> String {
+    self.parse(line);
+
+    return self.as_binary();
+  }
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let file_path = &args[1];
+
+  run(file_path);
 }
 
 pub fn run(file_path: &str) {
   let mut result: String = String::new();
 
-    if let Ok(lines) = read_lines(file_path) {
-        for ok_line in lines {
-            if let Ok(mut line) = ok_line {
-                line = line.trim().to_string();
-                if !line.starts_with(';') && line.len() > 0 {
-                    result.push_str(&process_line(line));
-                    result.push_str("\n");
-                }
-            }
-        }
-    }
+  if let Ok(lines) = read_lines(file_path) {
+      for ok_line in lines {
+          if let Ok(mut line) = ok_line {
+              line = line.trim().to_string();
+              if !line.starts_with(';') && line.len() > 0 {
+                  result.push_str(&process_line(line));
+                  result.push_str("\n");
+              }
+          }
+      }
+  }
 
-    fs::write("out.bin", result).expect("Unable to write file");
+  fs::write("out.bin", result).expect("Unable to write file");
 }
 
 fn process_line(line: String) -> String {
-    let parts: Vec<&str> = line.split(' ').collect();
+  let mut ins = Instruction::default();
+  ins.parse_opcode_from_line(line.clone());
 
-    if !OPCODES.contains_key(parts[0]) {
-        println!("Failed to parse, invalid opcode {}", parts[0]);
-        exit(1);
-    }
-
-    let opcode = OPCODES[parts[0]];
-    let var = VARS[&parts[1].to_lowercase()];
-    let types = TYPE[parts[2]];
-    let mut num : String = String::new();
-
-    if parts[3].starts_with('-') {
-        num = String::from("1");
-    }
-
-    num.push_str(&format!("{:08b}", parts[3].parse::<i32>().unwrap()));
-
-    return String::from(format!("{opcode}{var}{types}{num}"));
+  // This leaves things open to having shorter versions of assembly instructions (ex: PRT $0)
+  return match ins.opcode {
+    _ => ins.line_to_binary(line),
+  }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+  let file = File::open(filename)?;
+  Ok(io::BufReader::new(file).lines())
 }
